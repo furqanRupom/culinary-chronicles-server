84cd9f58ff941cd4d5c20dfb40b6fd1d057b6c47
rupom <furqanrupom978@gmail.com>
2023-05-02 20:03:39 +0600

    added cors for serve data to the client side

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express()
 const port = process.env.PORT || 4000
+const cors = require('cors')
 app.get('/',(req,res)=>{
     res.send('hello people')
 })
-
+app.use(cors())
 
 app.listen(port,()=>{
     console.log(`Culinary Chronicles website is running on port ${port}`)

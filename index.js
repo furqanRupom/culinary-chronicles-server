ba56e6811e271d193e7a6cfe8e1740a5d6bf33c8
rupom <furqanrupom978@gmail.com>
2023-05-02 20:10:02 +0600

    create api for serve data in client side

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const app = express()
-const port = process.env.PORT || 4000
+const app = express();
 const cors = require('cors')
-app.get('/',(req,res)=>{
-    res.send('hello people')
-})
+const chefInformation = require('./chef/chef')
+const port = process.env.PORT || 4000
 app.use(cors())
+app.get('/',(req,res)=>{
+    res.send(chefInformation);
+})
 
 app.listen(port,()=>{
     console.log(`Culinary Chronicles website is running on port ${port}`)
